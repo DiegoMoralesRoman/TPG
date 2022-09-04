401587242272f8851f1b92ac84358a05f14a9c9f
Diego Morales Román <moralesromandiego@gmail.com>
2022-09-05 01:05:54 +0200

    Fixed gitignore to delete build files

--- build/tpg/CMakeFiles/tpg.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libtpg.a"
-  "libtpg.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tpg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
